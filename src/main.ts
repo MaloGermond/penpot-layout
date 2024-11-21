1143a1c5edf77d10ad902a5ae1972fe9c8b041c7
MaloGermond <github@matchalo.org>
2024-11-21 19:39:46 +0100

    🎨 Add lot of feature

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,41 @@ import "./style.css";
 const searchParams = new URLSearchParams(window.location.search);
 document.body.dataset.theme = searchParams.get("theme") ?? "light";
 
-document.querySelector("[data-handler='create-text']")?.addEventListener("click", () => {
+document.querySelector("[data-handler='create-guides']")?.addEventListener("click", () => {
   // send message to plugin.ts
-  parent.postMessage("create-text", "*");
+  parent.postMessage("create-guides", "*");
+});
+
+document.querySelector("[data-handler='clear-guides']")?.addEventListener("click", () => {
+  // send message to plugin.ts
+  parent.postMessage("clear-guides", "*");
+});
+
+document.querySelector("[data-handler='fill-horizontal']")?.addEventListener("click", () => {
+  // send message to plugin.ts
+  parent.postMessage("fill-horizontal", "*");
+});
+
+document.querySelector("[data-handler='fill-vertical']")?.addEventListener("click", () => {
+  // send message to plugin.ts
+  parent.postMessage("fill-vertical", "*");
+});
+
+document.querySelector("[data-handler='invert-colors']")?.addEventListener("click", () => {
+  // send message to plugin.ts
+  parent.postMessage("invert-colors", "*");
+});
+
+
+
+document.querySelector("[data-handler='fill-both']")?.addEventListener("click", () => {
+  // send message to plugin.ts
+  parent.postMessage("fill-both", "*");
+});
+
+document.querySelector("[data-handler='log-selection']")?.addEventListener("click", () => {
+  // send message to plugin.ts
+  parent.postMessage("log-selection", "*");
 });
 
 // Listen plugin.ts messages
--- src/plugin.ts
+++ src/plugin.ts
@@ -1,17 +1,153 @@
+import {RulerGuide} from "@penpot/plugin-types";
+
 penpot.ui.open("Penpot plugin starter template", `?theme=${penpot.theme}`);
 
 penpot.ui.onMessage<string>((message) => {
-  if (message === "create-text") {
-    const text = penpot.createText("Hello world!");
+  console.log({message});
+  const selection = penpot.selection
+  const page = penpot.currentPage;
+  // Vérifiez si `page` n'est pas `null` avant d'accéder à ses propriétés
 
-    if (text) {
-      text.x = penpot.viewport.center.x;
-      text.y = penpot.viewport.center.y;
+  if (message === "create-guides") {
+    if(page === null){
+      console.error("page not found");
+      return;
+    }
 
-      penpot.selection = [text];
+    const guides = page.rulerGuides;
+    console.log(guides)
+
+    if(selection.length > 0) {
+      selection.map(el=>{
+        const x = el.x;
+        const y = el.y;
+        const w = el.width;
+        const h = el.height;
+
+        // Fonction pour vérifier si un guide existe déjà à une position donnée
+        const guideExists = (orientation, position) => {
+          return page.rulerGuides.some(guide => guide.orientation === orientation && guide.position === position);
+        };
+
+        // Ajouter les guides uniquement s'ils n'existent pas déjà
+        if (!guideExists("horizontal", y)) {
+          page.addRulerGuide("horizontal", y);
+        }
+        if (!guideExists("horizontal", y + h)) {
+          page.addRulerGuide("horizontal", y + h);
+        }
+        if (!guideExists("vertical", x)) {
+          page.addRulerGuide("vertical", x);
+        }
+        if (!guideExists("vertical", x + w)) {
+          page.addRulerGuide("vertical", x + w);
+        }
+      })
     }
   }
+  if (message === "clear-guides") {
+    if(page === null){
+      console.error("page not found");
+      return;
+    }
+    const guides = page.rulerGuides;
+
+    if (page && guides.length > 0) {
+      guides.map(el => page.removeRulerGuide(el))
+    }
+  }
+
+  if(message === "fill-horizontal" || "fill-vertical" || "fill-both" && selection.length > 0) {
+    const horizontal: boolean = message === "fill-horizontal" || message === "fill-both";
+    const vertical: boolean = message === "fill-vertical" || message === "fill-both";
+
+    selection.map(el=>{
+      if(el.type === "board"){
+        const childs= el.getChildren()
+
+        childs.map(item=>{
+          horizontal ? item.layoutChild.horizontalSizing = "fill" : null
+          vertical ? item.layoutChild.verticalSizing = "fill" : null
+        })
+      }
+      if(el.layoutChild != null){
+        horizontal ? el.layoutChild.horizontalSizing = "fill":null
+        vertical ? el.layoutChild.verticalSizing = "fill":null
+      }
+    })
+
+  }
+
+    /*
+    // Crée un flex layout dans le cas ou il n'existe pas.
+
+    const board = penpot.createBoard();
+
+    selection.map(el=>{board.appendChild(el)})
+
+    // Then you can add the flexLayout to it
+    const flex = board.addFlexLayout();
+    board.horizontalSizing = "auto";
+    board.verticalSizing = "auto";
+
+
+    // We can choose whether to go right or left with "row" or "row-reverse", or up or down with "column" or "column-reverse". The default is null.
+    flex.dir = "column";
+
+    // Next, we can decide the wrapping behaviour ("wrap" | "nowrap"). The default setting for this property is "undefined".
+    flex.wrap = "wrap";
+
+    // Let's align the items. These are the options: "start" | "end" | "center" | "stretch";
+    flex.alignItems = "center";
+    // and also we can align the content. These are the options "start" | "end" | "center" | "space-between" | "space-around" | "space-evenly" | "stretch";
+    flex.justifyContent = "center";
+
+    // We can set up a padding
+    flex.verticalPadding = 5;
+    flex.horizontalPadding = 5;
+
+    // We can choose how big the container is going to be, both horizontally and vertically. These are the options: "auto" | "fill" | "fix";
+    flex.horizontalSizing = "fill";
+    flex.verticalSizing = "fill";
+  */
+
+  if (message === "invert-colors") {
+
+    const fills = selection[0].fills
+    const strokes = selection[0].strokes
+
+    console.log(fills)
+    console.log(strokes)
+
+    selection[0].strokes = fills.map((color,index) => {
+
+      if(!color.fillColor){
+        return {strokeOpacity: color.fillOpacity, strokeStyle: 'solid', strokeWidth: index+1, strokeColorGradient: color.fillColorGradient, strokeAlignment: 'inner'}
+      }
+      return {strokeOpacity: color.fillOpacity, strokeStyle: 'solid', strokeWidth: index+1, strokeColor: color.fillColor, strokeAlignment: 'inner'}
+    })
+
+    selection[0].fills = strokes.map((color,index) => {
+      //console.log(color)
+      if(!color.strokeColor){
+        return {fillColorGradient: color.strokeColorGradient}
+      }
+      return {fillColor: color.strokeColor, fillOpacity: color.strokeOpacity}
+    })
+
+    /*
+    selection[0].strokes = fills.map((color,index) => {strokeOpacity: 1, strokeStyle: 'solid', strokeWidth: 1, strokeColor: color, strokeAlignment: 'inner'})
+    selection[0].fills = strokes.map((color,index) => {fillColor: color, fillOpacity: 1})
+    */
+
+  }
+
+  if(message === "log-selection") {
+    console.log(selection)
+  }
+
 });
+
 
 // Update the theme in the iframe
 penpot.on("themechange", (theme) => {
